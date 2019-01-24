e7c232363772269511101f1538fedef4e06fc6f5
Zachary Lehmann <zlehmann@gmail.com>
2019-01-24 18:15:08 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,7 +13,7 @@ class RecipesController < ApplicationController
 
   def create
     recipe = Recipe.create(recipe_params)
-    redirect_to recipe_path
+    redirect_to recipe_path(recipe)
   end
 
   private
